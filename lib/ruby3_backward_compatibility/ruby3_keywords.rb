ea6c2b4af44aafaf13fc4458e5bdd42d4036c2c5
Tobias Kraze <tobias.kraze@makandra.de>
2022-10-25 17:03:16 +0200

    ruby3_keywords does not change visibility of private / protected methods

--- lib/ruby3_backward_compatibility/ruby3_keywords.rb
+++ lib/ruby3_backward_compatibility/ruby3_keywords.rb
@@ -2,11 +2,19 @@ module Ruby3BackwardCompatibility
   module Ruby3Keywords
     def ruby3_keywords(*methods)
       methods.each do |method|
+        method_object = instance_method(method)
+
         _ruby3_keywords_module.define_method(method) do |*args, **keyword_args|
           if args.last.is_a?(Hash)
             keyword_args.merge!(args.pop)
           end
           super(*args, **keyword_args)
+        end
+
+        if method_object.private?
+          _ruby3_keywords_module.send(:private, method)
+        elsif method_object.protected?
+          _ruby3_keywords_module.send(:protected, method)
         end
       end
     end
--- spec/ruby3_backward_compatibility/ruby3_keywords_spec.rb
+++ spec/ruby3_backward_compatibility/ruby3_keywords_spec.rb
@@ -11,6 +11,16 @@ module Ruby3BackwardCompatibility
     def keyword_method_3(regular_arg, keyword_arg:)
       [regular_arg, keyword_arg]
     end
+
+    private
+
+    def private_method(regular_arg, keyword_arg:)
+      [regular_arg, keyword_arg]
+    end
+
+    def protected_method(regular_arg, keyword_arg:)
+      [regular_arg, keyword_arg]
+    end
   end
 
   class Ruby3Class
@@ -18,6 +28,8 @@ module Ruby3BackwardCompatibility
 
     ruby3_keywords :keyword_method
     ruby3_keywords :keyword_method_2, :keyword_method_3
+
+    ruby3_keywords :private_method
   end
 
   describe Ruby3Keywords do
@@ -33,6 +45,28 @@ module Ruby3BackwardCompatibility
       it 'can loop' do
         expect(Ruby3Class.new.keyword_method_2('foo', { keyword_arg: 'bar' })).to eq(['foo', 'bar'])
         expect(Ruby3Class.new.keyword_method_3('foo', { keyword_arg: 'bar' })).to eq(['foo', 'bar'])
+      end
+
+      context 'on private methods' do
+        it 'works' do
+          expect(Ruby3Class.new.send(:private_method, 'foo', keyword_arg: 'bar')).to eq(['foo', 'bar'])
+        end
+
+        it 'does not make the method public' do
+          expect(Ruby3Class.new.respond_to?(:private_method)).to eq(false)
+          expect(Ruby3Class.new.respond_to?(:private_method, true)).to eq(true)
+        end
+      end
+
+      context 'on protected methods' do
+        it 'works' do
+          expect(Ruby3Class.new.send(:protected_method, 'foo', keyword_arg: 'bar')).to eq(['foo', 'bar'])
+        end
+
+        it 'does not make the method public' do
+          expect(Ruby3Class.new.respond_to?(:protected_method)).to eq(false)
+          expect(Ruby3Class.new.respond_to?(:protected_method, true)).to eq(true)
+        end
       end
     end
   end
