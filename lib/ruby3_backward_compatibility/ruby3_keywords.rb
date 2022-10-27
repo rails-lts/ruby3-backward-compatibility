7e8decbd1e9a19a391e563a2c7ba90503aa9975b
Tobias Kraze <tobias.kraze@makandra.de>
2022-10-27 15:34:34 +0200

    fix tests

--- lib/ruby3_backward_compatibility/ruby3_keywords.rb
+++ lib/ruby3_backward_compatibility/ruby3_keywords.rb
@@ -6,23 +6,25 @@ module Ruby3BackwardCompatibility
       by.send(:_ruby3_keywords_module)
     end
 
-    def ruby3_keywords(*methods)
-      methods.each do |method|
-        method_is_private = private_instance_methods.include?(method)
-        method_is_protected = protected_instance_methods.include?(method)
+    def ruby3_keywords(*method_names)
+      method_names.each do |method_name|
+        method_is_private = private_instance_methods.include?(method_name)
+        method_is_protected = protected_instance_methods.include?(method_name)
 
-        arity_before_prepend = self.instance_method(method).arity
-        _ruby3_keywords_module.define_method(method) do |*args, **keyword_args|
-          if args.last.respond_to?(:to_hash) && args.size > (arity_before_prepend.abs-1)
-            keyword_args.merge!(args.pop)
+        required_param_count = instance_method(method_name).parameters.sum { |(kind, _name)| kind == :req ? 1 : 0 }
+        _ruby3_keywords_module.define_method(method_name) do |*args|
+          if args.last.respond_to?(:to_hash) && args.size > required_param_count
+            keyword_args = args.pop
+            super(*args, **keyword_args)
+          else
+            super(*args)
           end
-          super(*args, **keyword_args)
         end
 
         if method_is_private
-          _ruby3_keywords_module.send(:private, method)
+          _ruby3_keywords_module.send(:private, method_name)
         elsif method_is_protected
-          _ruby3_keywords_module.send(:protected, method)
+          _ruby3_keywords_module.send(:protected, method_name)
         end
       end
     end
