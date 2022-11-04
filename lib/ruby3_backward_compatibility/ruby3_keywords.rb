2bc93b872d4cf5f21baf225935ef3f8b4de627c4
Tobias Kraze <tobias.kraze@makandra.de>
2022-11-04 15:45:18 +0100

    add a warning message when calling ruby3_keywords on wrong owner

--- lib/ruby3_backward_compatibility/ruby3_keywords.rb
+++ lib/ruby3_backward_compatibility/ruby3_keywords.rb
@@ -1,11 +1,12 @@
 module Ruby3BackwardCompatibility
   module Ruby3Keywords
-    def self.find_own_instance_method(mod, method_name)
+    def self.find_owned_instance_method(mod, method_name)
       method = mod.send(:instance_method, method_name)
       while method.owner > mod
         # we found the method in a prepended module
         super_method = method.super_method
         if super_method.nil?
+          warn "Called `ruby3_keywords #{method_name.inspect}` on `#{mod}`, which appears not to be the correct owner. Did you mean to call it on `#{method.owner}`?"
           return method
         else
           method = super_method
@@ -25,7 +26,7 @@ module Ruby3BackwardCompatibility
         method_is_private = private_instance_methods.include?(method_name)
         method_is_protected = protected_instance_methods.include?(method_name)
 
-        required_param_count = Ruby3Keywords.find_own_instance_method(self, method_name).parameters.sum { |(kind, _name)| kind == :req ? 1 : 0 }
+        required_param_count = Ruby3Keywords.find_owned_instance_method(self, method_name).parameters.sum { |(kind, _name)| kind == :req ? 1 : 0 }
         _ruby3_keywords_module.define_method(method_name) do |*args|
           if args.last.respond_to?(:to_hash) && args.size > required_param_count
             keyword_args = args.pop
