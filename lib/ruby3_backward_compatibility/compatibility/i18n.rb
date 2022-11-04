0a9cdcab1685edcba4c014b6d88adb743b1477a7
Tobias Kraze <tobias.kraze@makandra.de>
2022-11-04 16:30:15 +0100

    avoid warning message for I18n fixes

--- lib/ruby3_backward_compatibility/compatibility/i18n.rb
+++ lib/ruby3_backward_compatibility/compatibility/i18n.rb
@@ -1,9 +1,7 @@
 require 'i18n'
 
-module I18n
-  class << self
-    extend Ruby3BackwardCompatibility::Ruby3Keywords
+module I18n::Base
+  extend Ruby3BackwardCompatibility::Ruby3Keywords
 
-    ruby3_keywords :translate, :translate!, :localize, :t, :l, :transliterate
-  end
+  ruby3_keywords :translate, :translate!, :localize, :t, :l, :transliterate
 end
