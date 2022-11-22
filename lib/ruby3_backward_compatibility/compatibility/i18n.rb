5cec800b2f1dde427c5daedc7b7bd33b9b5b4ebf
Tobias Kraze <tobias.kraze@makandra.de>
2022-11-22 10:47:41 +0100

    do not crash for old i18n

--- lib/ruby3_backward_compatibility/compatibility/i18n.rb
+++ lib/ruby3_backward_compatibility/compatibility/i18n.rb
@@ -3,5 +3,5 @@ require 'i18n'
 module I18n::Base
   extend Ruby3BackwardCompatibility::Ruby3Keywords
 
-  ruby3_keywords :translate, :translate!, :localize, :t, :l, :transliterate
+  ruby3_keywords :translate, :translate!, :localize, :t, :l, :transliterate, ignore_missing: true
 end
