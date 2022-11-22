f93518e3f742b9fcc2ecab0af3b41aea622ed517
Tobias Kraze <tobias.kraze@makandra.de>
2022-11-22 16:03:40 +0100

    do not alias Psych.load to Psych.safe_load (!!)

--- spec/ruby3_backward_compatibility/compatibility/psych_spec.rb
+++ spec/ruby3_backward_compatibility/compatibility/psych_spec.rb
@@ -2,16 +2,8 @@ require 'ruby3_backward_compatibility/compatibility/psych'
 
 module Ruby3BackwardCompatibility
   describe Psych do
-    describe '#load' do
-      it 'behaves like unsafe_load' do
-        unsafe_object = { t: Time.now }
-        expect(Psych.load(unsafe_object.to_yaml)).to eq(unsafe_object)
-      end
-    end
-
     describe '#safe_load' do
       it 'can be called with old style positional arguments' do
-        p Psych::VERSION.class
         object = { 't' => Time.now, 'k' => :foo }
         expect(Psych.safe_load(object.to_yaml, [Symbol, Time], [:foo])).to eq(object)
 
