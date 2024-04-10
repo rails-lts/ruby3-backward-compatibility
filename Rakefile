a745864101f90209b5d94c4f8c4d6ef43325cb99
Tobias Kraze <tobias.kraze@makandra.de>
2024-04-10 15:09:44 +0200

    rework how the spec for compatibility/all works

--- Rakefile
+++ Rakefile
@@ -5,10 +5,16 @@ require 'rspec/core/rake_task'
 
 ruby2_specs = 'spec/ruby2/**/*_spec.rb'
 rake_task = RSpec::Core::RakeTask.new(:spec)
+
 if RUBY_VERSION < '3'
-  rake_task.pattern = ruby2_specs
+  rake_task.pattern = 'spec/ruby2/**/*_spec.rb'
+  task default: :spec
 else
-  rake_task.exclude_pattern = ruby2_specs
+  rake_task.exclude_pattern = 'spec/{ruby2,isolated}/**/*_spec.rb'
+
+  isolated_rake_task = RSpec::Core::RakeTask.new(:isolated_specs)
+  isolated_rake_task.pattern = 'spec/isolated/**/*_spec.rb'
+
+  task default: [:spec, :isolated_specs]
 end
 
-task default: :spec
---
+++ spec/isolated/ruby3_backward_compatibility/compatibility/all_spec.rb
@@ -0,0 +1,24 @@
+require 'psych'
+require 'i18n'
+require 'uri'
+require 'ruby3_backward_compatibility/compatibility/all'
+
+module Ruby3BackwardCompatibility
+  describe 'all' do
+    def expand_require(path)
+      File.expand_path(File.join(__dir__, '..', '..', '..', '..', 'lib', "#{path}.rb"))
+    end
+
+    it 'requires everything' do
+      expect($LOADED_FEATURES).to include(expand_require('ruby3_backward_compatibility/compatibility/dir'))
+      expect($LOADED_FEATURES).to include(expand_require('ruby3_backward_compatibility/compatibility/erb'))
+      expect($LOADED_FEATURES).to include(expand_require('ruby3_backward_compatibility/compatibility/file'))
+      expect($LOADED_FEATURES).to include(expand_require('ruby3_backward_compatibility/compatibility/fixnum'))
+      expect($LOADED_FEATURES).to include(expand_require('ruby3_backward_compatibility/compatibility/i18n'))
+      expect($LOADED_FEATURES).to include(expand_require('ruby3_backward_compatibility/compatibility/object'))
+      expect($LOADED_FEATURES).to include(expand_require('ruby3_backward_compatibility/compatibility/psych'))
+      expect($LOADED_FEATURES).to include(expand_require('ruby3_backward_compatibility/compatibility/string'))
+      expect($LOADED_FEATURES).to include(expand_require('ruby3_backward_compatibility/compatibility/uri'))
+    end
+  end
+end
